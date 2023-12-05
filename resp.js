7ae926e4c12dfd44dfab366226eb21c94b66dd76
Anuprita <anupritamhapankar22@gmail.com>
2023-12-05 19:11:04 +0530

    updated commit

--- resp.js
+++ resp.js
@@ -1,4 +1,6 @@
 function toggleMenu() {
     var navbar = document.querySelector(".navbar");
     navbar.style.display = (navbar.style.display === "flex") ? "none" : "flex";
+
+    event.preventDefault();
 }
